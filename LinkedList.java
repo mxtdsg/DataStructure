b90f90450a00d3bb6b5cdac597d0d1c71d698e51
Xintian Meng <mxtdsg@hotmail.com>
2017-10-11 21:50:04 -0700

    setup linked list

---
+++ LinkedList.java
@@ -0,0 +1,32 @@
+
+
+public class LinkedList {
+    class Node {
+        int value;
+        Node next;
+        Node(int integer) {
+            this.value = integer;
+            this.next = null;
+        }
+    }
+
+    Node firstNode;
+    Node lastNode;
+    int size;
+
+    public int size() {
+        return size;
+    }
+
+    public void add() {
+
+    }
+
+    public void remove() {
+
+    }
+
+    public Node get(int i) {
+
+    }
+}
